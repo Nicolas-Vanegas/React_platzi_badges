e247d33bd6f449ad145d2478bc0c057d8f161fc5
Nicolás Vanegas <Nicolas.vanegas.p@hotmail.com>
2020-09-29 17:40:55 -0500

    Creado el primer hook de prueba

--- src/pages/BadgeDetails.js
+++ src/pages/BadgeDetails.js
@@ -7,8 +7,25 @@ import confLogo from "../images/platziconf-logo.svg";
 import Badge from "../components/Badge";
 import DeleteBadgeModal from "../components/DeleteBadgeModal";
 
-//Usamos función cuando no usamos lógica
+//Usamos función cuando no usamos lógica. Cuando son funciones es porque ese componente no tiene un estado propio que manejar, no tiene ciclos de vida.
+//Cuando necesitamos que las funciones actuen como clases usamos los HOOKS
+//Crear un custom hook nos srive para encapsular lógica específica dentro de una función
+//Crearé un hook el cual SIEMPRE debe comenzar por la palabra use
+function useIncreaseCount(max) {
+  //Esto es para usar Hooks, el useState (con un parámetro para inicializar el state) es un hook Para manejar el estado. Que regresa un arreglo. lo que está dentro del arreglo en este caso viene siendo como state y setState pero en una función.
+  const [count, setCount] = React.useState(0);
+
+  //Si en count es mayor al parametro de useIncreaseCount(5 en este caso) vuelve el count a 0
+  if (count > max) {
+    setCount(0);
+  }
+
+  //Siempre regresamos estos valores para que lo reciba la función BadgeDetails
+  return [count, setCount];
+}
+
 function BadgeDetails(props) {
+  const [count, setCount] = useIncreaseCount(5);
   const badge = props.badge;
   return (
     <div>
@@ -43,6 +60,14 @@ function BadgeDetails(props) {
             <h2>Actions</h2>
             <div>
               <div>
+                <button
+                  onClick={() => {
+                    setCount(count + 1);
+                  }}
+                  className="btn btn-primary mr-4 "
+                >
+                  Increase Count: {count}
+                </button>
                 <Link className="btn btn-primary mb-4" to={`/badges/${badge.id}/edit`}>
                   Edit
                 </Link>
